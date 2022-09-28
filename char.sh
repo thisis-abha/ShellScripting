357b3d0fe881f9d435fb74fdaff12c1a84d108de
abhabhargava <abha.bhargava@nagarro.com>
2022-09-28 12:45:26 +0530

    Modified

--- char.sh
+++ char.sh
@@ -1,6 +1,5 @@
 #!bin/bash
 
 # This script prints characters of input String
-echo "Enter Name :"
-read name
-echo $name | sed -e 's/\(.\)/\1\n/g'
+read -p "Enter String : " s
+echo $s | sed -e 's/\(.\)/\1\n/g'
